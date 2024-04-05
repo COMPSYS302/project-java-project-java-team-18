19f3c59e2ffab5c5d4954304e16dee85f454309e
saineeha <ndes239@aucklanduni.ac.nz>
2024-04-05 23:22:55 +1300

    restored main activity

---
+++ app/src/main/java/com/example/sneakrapp/activities/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.sneakrapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sneakrapp.R;
+import com.example.sneakrapp.helpers.DataProvider;
+
+public class MainActivity extends AppCompatActivity {
+
+    private class ViewHolder {
+        CardView cardviewNumbers;
+
+        public ViewHolder() {
+            cardviewNumbers = findViewById(R.id.cardViewNumbers);
+        }
+    }
+
+    ViewHolder vh;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vh = new ViewHolder();
+
+        vh.cardviewNumbers.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent designerActivity = new Intent(getBaseContext(), DesignerActivity.class);
+                designerActivity.putExtra("Message from main", "Hi");
+                startActivity(designerActivity);
+            }
+        });
+    }
+
+}
