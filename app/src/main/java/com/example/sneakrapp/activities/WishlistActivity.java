71de0e6163a3aef1bfa2d4e4d8c88591fb45e5ee
saineeha <ndes239@aucklanduni.ac.nz>
2024-04-06 23:07:29 +1300

    changed wishlist and designer activity

---
+++ app/src/main/java/com/example/sneakrapp/activities/WishlistActivity.java
@@ -0,0 +1,30 @@
+package com.example.sneakrapp.activities;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sneakrapp.ProductAdaptor;
+import com.example.sneakrapp.R;
+import com.example.sneakrapp.helpers.DataProvider;
+import com.example.sneakrapp.models.Product;
+
+import java.util.List;
+
+public class WishlistActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_designer);
+
+        List<Product> product = DataProvider.getProducts();
+
+        ProductAdaptor productAdaptor = new ProductAdaptor(this, R.layout.product_listview_item,
+                product);
+
+        ListView listView = findViewById(R.id.products_listview);
+        listView.setAdapter(productAdaptor);
+
+    }
+}
--- app/src/main/java/com/example/sneakrapp/helpers/DataProvider.java
+++ app/src/main/java/com/example/sneakrapp/helpers/DataProvider.java
@@ -2,35 +2,58 @@ package com.example.sneakrapp.helpers;
 
 import com.example.sneakrapp.models.Product;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class DataProvider {
-    public static Map<Integer, String> generateShoeProducts() {
-        Map<Integer, String> words =
-                new LinkedHashMap<Integer, String>();
-        words.put(1, "Shoe1");
-        words.put(2, "Shoe2");
-        words.put(3, "Shoe3");
-        words.put(4, "Shoe4");
-        words.put(5, "Shoe5");
-        words.put(6, "Shoe6");
-        words.put(7, "Shoe7");
-        words.put(8, "Shoe8");
-        words.put(9, "Shoe9");
-        return words;
-    }
+//    public static Map<Integer, String> generateShoeProducts() {
+//        Map<Integer, String> words =
+//                new LinkedHashMap<Integer, String>();
+//        words.put(1, "Shoe1");
+//        words.put(2, "Shoe2");
+//        words.put(3, "Shoe3");
+//        words.put(4, "Shoe4");
+//        words.put(5, "Shoe5");
+//        words.put(6, "Shoe6");
+//        words.put(7, "Shoe7");
+//        words.put(8, "Shoe8");
+//        words.put(9, "Shoe9");
+//        return words;
+//    }
+
+
+        public static Map<Integer, String> generateShoeProducts() {
+            Map<Integer, String> words = new LinkedHashMap<>();
+            List<String> shoeNames = new ArrayList<>(Arrays.asList(
+                    "x Travis Scott Air Jordan 4 Retro sneakers", "Designer Shoes",  "Running Shoes",
+                    "Casual Shoes",
+                    "Sports Shoes",
+                    "Formal Shoes",
+                    "Sneakers",
+                    "Boots",
+                    "Sandals",
+                    "Slippers"
+            ));
+
+            for (int i = 0; i < shoeNames.size(); i++) {
+                words.put(i + 1, shoeNames.get(i));
+            }
+            return words;
+        }
+
+
 
     public static List<Product> getProducts() {
         List<Product> productsList = new LinkedList<Product>();
         Map<Integer, String> words = generateShoeProducts();
         for (Integer key : words.keySet()) {
             int digit = key;
-            //String maoriTranslation = words.get(key);
             String text = words.get(key);
-            String icon = "icon" + String.valueOf(key);
+            String icon = "firstpic" + String.valueOf(key);
             String heart = "heart" + String.valueOf(key);
             Product n = new Product(digit, icon, text, heart);
             productsList.add(n);
